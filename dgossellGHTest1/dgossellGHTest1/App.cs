f1e275b97e6bcef223da4efdf032d784e258c326
htcstudent <htcstudent@EH255WD06>
2017-02-06 19:06:14 -0600

    Add project files.

---
+++ dgossellGHTest1/dgossellGHTest1/App.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace dgossellGHTest1
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            var content = new ContentPage
+            {
+                Title = "dgossellGHTest1",
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "This is my first test for uploading a project to Git Hub."
+                        },
+
+                        new Label
+                        {
+                            HorizontalTextAlignment = TextAlignment.Start,
+                            Text = "I hope it works."
+                        }
+                    }
+                }
+            };
+
+            MainPage = new NavigationPage(content);
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
